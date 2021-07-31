75e422106565a77809da54d9478501d820312e63
vivaan1304 <vivaangupta1304@gmail.com>
2021-07-31 23:38:38 +0530

    initial

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # dont track modifications to database(i.e. dont send notification that change has been made)
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25 )
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['vivaangupta1304@gmail.com'] 
+    POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
